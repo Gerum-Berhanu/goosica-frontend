6e44ac4c53ac6d0c67f96f6eda0c2dc971fe7817
Gerum-Berhanu <gerumberhanu0@gmail.com>
2026-01-01 21:55:09 +0300

    avoided generateId for good

--- src/components/sections/SearchResult.tsx
+++ src/components/sections/SearchResult.tsx
@@ -1,12 +1,11 @@
 import { Card } from "../ui/Card";
-import { generateId } from "../utils/devToolkit";
 
 export function SearchResult() {
     return (
       <div className="flex flex-wrap justify-center gap-8 px-2 md:px-8 py-8">
-        {[...Array(7)].map((_) => (
+        {[...Array(7)].map((_, index) => (
           <Card
-            key={generateId()}
+            key={index}
             title="Zenith"
             uploader="Kavinsky"
             imagePath="./zenith-cover.jpg"
