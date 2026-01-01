7d876bd38329fdf8c2009f9643c4414ddb73f473
Gerum-Berhanu <gerumberhanu0@gmail.com>
2026-01-01 20:18:56 +0300

    search button displays search result field

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {CardSet} from "./CardSet";
 import { generateId } from "./devToolkit";
 import {SearchBar} from "./SearchBar";
@@ -5,20 +6,31 @@ import { SearchResult } from "./SearchResult";
 
 export function Body() {
     const CardSetTitles = ["Today's Pick", "Queue", "Favorites", "Downloads"];
-    
+    const [ isSearched, setIsSearched ] = useState(false);
+
+    const handleSearch = (query: string | undefined) => {
+      if (!query)
+        return;
+
+      // fetching data based on the provided query
+
+      setIsSearched(true);
+    }
+
     return (
       <div className="bg-slate-200 flex flex-col h-full overflow-y-auto w-full md:w-2/3 lg:w-3/4">
-        <SearchBar />
+        <SearchBar onSearch={handleSearch}/>
         <div className="h-full overflow-y-auto">
-          {false ? ( // CardSet
+          {isSearched ? (
+            // SearchResult
+            <SearchResult />
+          ) : (
+            // CardSet
             <div className="flex flex-col gap-8 px-2 md:px-8 py-8">
               {CardSetTitles.map((item) => (
                 <CardSet key={generateId()} title={item} />
               ))}
             </div>
-          ) : (
-            // SearchResult
-            <SearchResult />
           )}
         </div>
       </div>
